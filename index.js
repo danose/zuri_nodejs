858ebae9329fbdffeb61d151ddd580cec7caca17
unknown <etiobhiodaniel@gmail.com>
2022-06-18 03:07:12 +0100

    nodejs http server

--- index.js
+++ index.js
@@ -1,23 +1,6 @@
 var http = require('http');
-var fs = require('fs');
+var app = require('./app');
 
-http.createServer(function (request, response){
-    
-    response.writeHead(200, {'Content-Type':'text/html'});
-
-    fs.readFile('home.html',null,function(error,data){
-
-        if(error){
-            response.writeHead(404);
-            response.write("file not found");
-        }
-        else{
-            response.write(data);
-        }
-
-        response.end();
-    })
-    
-}).listen(8081);
+http.createServer(app.handleRequest).listen(8081);
 
 console.log('Server running at port 8081');
